c3bae6e596fa25c8302ed9397ea65d73a7e009c6
7Loaded <48351115+DhruvalBhinsara1@users.noreply.github.com>
2020-12-16 15:56:23 +0530

    Update settings.py

--- notekeeper/notekeeper/settings.py
+++ notekeeper/notekeeper/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('!fywv%ZV3mg![ny')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['keep-notes-by-dhruval-bhinsara.herokuapp.com','127.1.0.0:8000','127.1.0.0:5000']
 
 
 # Application definition
